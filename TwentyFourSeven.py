38f0f8f08e0d1bd9299db51fafd008a6e4ca4e1f
AnandakrishnanV <iamanandakrishnanv@gmail.com>
2023-03-10 14:07:00 +0800

    added four-twenty, solver

--- TwentyFourSeven.py
+++ TwentyFourSeven.py
@@ -37,6 +37,19 @@ s = Solver()
 
 X = [[Int("x_%s_%s" % (i + 1, j + 1)) for j in range(12)] for i in range(12)]
 
+def row_col_four_twenty(grid,c):
+    sum_of_rows([row[:7] for row in grid[:7]], s)
+    four_in_each_row([row[:7] for row in grid[:7]], s)
+
+    sum_of_rows([row[5:] for row in grid[:7]], s)
+    four_in_each_row([row[5:] for row in grid[:7]], s)
+
+    sum_of_rows([row[:7] for row in grid[5:]], s)
+    four_in_each_row([row[:7] for row in grid[5:]], s)
+
+    sum_of_rows([row[5:] for row in grid[5:]], s)
+    four_in_each_row([row[5:] for row in grid[5:]], s)
+
 def four_in_each_row(grid,c):
     # print_grid(grid)
     for i in range(7):
@@ -52,7 +65,28 @@ def sum_of_rows(grid, c):
     for j in range(7):
         c.add(Sum([If(grid[i][j] != 0,grid[i][j],0) for i in range(7) ])==20)
 
+def blue_constraints(grid,c):
+    #left vertical
+    for i in range(12):
+        row_sum = z3.Sum(grid[i])
+        c.add(Or)
 
 
-constrs = None
-sum_of_rows([row[:7] for row in X[:7]], constrs)
+
+s = Solver()
+row_col_four_twenty(X,s)
+
+tfs_array_constr = [ If(tfs_array[i][j] == 0, 
+                  True, 
+                  X[i][j] == tfs_array[i][j]) 
+               for i in range(12) for j in range(12) ]
+
+s.add(tfs_array_constr)
+
+if s.check() == sat:                                    # (3)
+    m = s.model()                                       # (4)
+    r = [ [ m.evaluate(X[i][j]) for j in range(12) ]     # (5)
+          for i in range(12) ]
+    print_grid(r)                                     # (6)
+else:
+    print("failed to solve")                            # (7)
