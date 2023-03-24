29131e258a859f1fca1393a8d4f7bcdf4ccb1560
AnandakrishnanV <iamanandakrishnanv@gmail.com>
2023-03-24 12:53:00 +0800

    changed blue constraint logic to be fully based on input parameters
    supports upto 9 zeros per row

--- TwentyFourSeven.py
+++ TwentyFourSeven.py
@@ -102,66 +102,104 @@ def sum_of_rows(grid, c):
               for i in range(7)]) == 20)
 
 
-def blue_constraints(grid, c):
+def blue_constraints(grid, c, n, z):
+    z = z - 1
+    mi = n - 1
     # left vertical
-    for i in range(12):
+    for i in range(n):
         row_sum = z3.Sum(grid[i])
 
         if row_constr_left[i]:
-            first_val = z3.If(
-                grid[i][0] == 0,
-                If(
-                    grid[i][1] == 0,
-                    If(grid[i][2] == 0, grid[i][3], grid[i][2]),
-                    grid[i][1],
-                ),
-                grid[i][0],
-            )
+            first_val = z3.If(And(z > 0, grid[i][0] == 0),
+                              If(And(z > 1, grid[i][1] == 0),
+                                 If(And(z > 2, grid[i][2] == 0),
+                                    If(And(z > 3, grid[i][3] == 0),
+                                        If(And(z > 4, grid[i][4] == 0),
+                                           If(And(z > 5, grid[i][5] == 0),
+                                              If(And(z > 6, grid[i][6] == 0),
+                                                 If(And(
+                                                     z > 7, grid[i][7] == 0), grid[i][8], grid[i][7]),
+                                                 grid[i][6]),
+                                              grid[i][5]),
+                                           grid[i][4]),
+                                       grid[i][3]),
+                                    grid[i][2],),
+                                 grid[i][1],),
+                              grid[i][0],
+                              )
+
             c.add(
                 Or(first_val == row_constr_left[i], row_sum == row_constr_left[i]))
 
         if row_constr_right[i]:
-            first_val = z3.If(
-                grid[i][11] == 0,
-                If(
-                    grid[i][10] == 0,
-                    If(grid[i][9] == 0, grid[i][8], grid[i][9]),
-                    grid[i][10],
-                ),
-                grid[i][11],
-            )
+            first_val = z3.If(And(z > 0, grid[i][mi-0] == 0),
+                              If(And(z > 1, grid[i][mi-1] == 0),
+                                 If(And(z > 2, grid[i][mi-2] == 0),
+                                    If(And(z > 3, grid[i][mi-3] == 0),
+                                        If(And(z > 4, grid[i][mi-4] == 0),
+                                           If(And(z > 5, grid[i][mi-5] == 0),
+                                              If(And(z > 6, grid[i][mi-6] == 0),
+                                                 If(And(z > 7, grid[i][mi-7] == 0), grid[i][mi-8], grid[i][mi-7]),
+                                                 grid[i][mi-6]),
+                                              grid[i][mi-5]),
+                                           grid[i][mi-4]),
+                                       grid[i][mi-3]),
+                                    grid[i][mi-2],),
+                                 grid[i][mi-1],),
+                              grid[i][mi-0],
+                              )
+            
+
             c.add(
                 Or(first_val == row_constr_right[i], row_sum == row_constr_right[i]))
 
     # columns
-    for j in range(12):
-        col_sum = z3.Sum([grid[i][j] for i in range(12)])
+    for j in range(n):
+        col_sum = z3.Sum([grid[i][j] for i in range(n)])
 
         if column_constr_top[j]:
-            first_val = z3.If(
-                grid[0][j] == 0,
-                If(
-                    grid[1][j] == 0,
-                    If(grid[2][j] == 0, grid[3][j], grid[2][j]),
-                    grid[1][j],
-                ),
-                grid[0][j],
-            )
+            first_val = z3.If(And(z > 0, grid[0][j] == 0),
+                              If(And(z > 1, grid[1][j] == 0),
+                                 If(And(z > 2, grid[2][j] == 0),
+                                    If(And(z > 3, grid[3][j] == 0),
+                                        If(And(z > 4, grid[4][j] == 0),
+                                           If(And(z > 5, grid[5][j] == 0),
+                                              If(And(z > 6, grid[6][j] == 0),
+                                                 If(And(
+                                                     z > 7, grid[7][j] == 0), grid[8][j], grid[7][j]),
+                                                 grid[6][j]),
+                                              grid[5][j]),
+                                           grid[4][j]),
+                                       grid[3][j]),
+                                    grid[2][j],),
+                                 grid[1][j],),
+                              grid[0][j],
+                              )
+
             c.add(
                 Or(first_val == column_constr_top[j],
                    col_sum == column_constr_top[j])
             )
 
         if column_constr_bottom[j]:
-            first_val = z3.If(
-                grid[11][j] == 0,
-                If(
-                    grid[10][j] == 0,
-                    If(grid[9][j] == 0, grid[8][j], grid[9][j]),
-                    grid[10][j],
-                ),
-                grid[11][j],
-            )
+            first_val = z3.If(And(z > 0, grid[mi-0][j] == 0),
+                              If(And(z > 1, grid[mi-1][j] == 0),
+                                 If(And(z > 2, grid[mi-2][j] == 0),
+                                    If(And(z > 3, grid[mi-3][j] == 0),
+                                        If(And(z > 4, grid[mi-4][j] == 0),
+                                           If(And(z > 5, grid[mi-5][j] == 0),
+                                              If(And(z > 6, grid[mi-6][j] == 0),
+                                                 If(And(
+                                                     z > 7, grid[mi-7][j] == 0), grid[mi-8][j], grid[mi-7][j]),
+                                                 grid[mi-6][j]),
+                                              grid[mi-5][j]),
+                                           grid[mi-4][j]),
+                                       grid[mi-3][j]),
+                                    grid[mi-2][j],),
+                                 grid[mi-1][j],),
+                              grid[mi-0][j],
+                              )
+            
             c.add(
                 Or(
                     first_val == column_constr_bottom[j],
@@ -199,7 +237,8 @@ def get_neightbours(grid, i, j):
 def enforce_cell_neighbours(grid, c):
     for i in range(len(grid)-1):
         for j in range(len(grid)-1):
-            c.add(Implies(grid[i][j] != 0, Or([k != 0 for k in get_neightbours(grid, i, j)])))
+            c.add(Implies(grid[i][j] != 0, Or(
+                [k != 0 for k in get_neightbours(grid, i, j)])))
 
 
 s = Solver()
@@ -208,7 +247,7 @@ X = [[Int("x_%s_%s" % (i + 1, j + 1)) for j in range(12)] for i in range(12)]
 
 one_to_seven(X, s)
 row_col_four_twenty(X, s)
-blue_constraints(X, s)
+blue_constraints(X, s, 12, 4)
 two_by_two_subgrid(X, s)
 
 tfs_array_constr = [
@@ -250,6 +289,7 @@ def is_fully_connected(matrix):
     # Check if there is only one connected component
     return connected_components == 1
 
+
 def connected_zeros(matrix):
     rows, cols = len(matrix), len(matrix[0])
     visited = [[False] * cols for _ in range(rows)]
@@ -284,18 +324,21 @@ def connected_zeros(matrix):
     # Check if there is only one connected component
     return len_connected_components
 
+
 def calculate_answer(grid):
-   print("Final Answer : ")
-   print(reduce((lambda x,y: x*y), connected_zeros(grid)))
-   print("Execution Complete!!")
+    print("Final Answer : ")
+    print(reduce((lambda x, y: x*y), connected_zeros(grid)))
+    print("Execution Complete!!")
+
 
 # enforce_cell_neighbours(X, s)
-stats_to_print = ["decisions","solve-eqs-steps","time","num allocs","memory"]
+stats_to_print = ["decisions", "solve-eqs-steps",
+                  "time", "num allocs", "memory"]
 start_time = time.time_ns()
 while True:
     if s.check() == sat:
         print("------------------------")
-        # print(s.statistics().keys()) 
+        # print(s.statistics().keys())
         m = s.model()
         print("stats for this run")
         stats = s.statistics()
@@ -303,7 +346,8 @@ while True:
             if stat in stats.keys():
                 print(stat, stats.get_key_value(stat))
 
-        r = [[m.evaluate(X[i][j]).as_long() for j in range(12)] for i in range(12)]
+        r = [[m.evaluate(X[i][j]).as_long() for j in range(12)]
+             for i in range(12)]
         if is_fully_connected(r):
             print("found solution")
             print_matrix(r)
@@ -314,7 +358,7 @@ while True:
             new_c = Not(And([X[i][j] == r[i][j]
                         for i in range(12) for j in range(12)]))
             s.add(new_c)
-        
+
     else:
         print("failed to solve")
         break
